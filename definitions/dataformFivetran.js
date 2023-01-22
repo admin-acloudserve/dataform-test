6fd5c8f2e40aee9d87e0f27e15858886bd123690
ADMIN ADMIN <admin@acloudserve.com>
2023-01-22 06:50:19 +0000

    interesting

---
+++ definitions/dataformFivetran.js
@@ -0,0 +1,12 @@
+// const fivetranLog = require("fivetran-log");
+
+// fivetranLog({
+//   // The name of your fivetran log schema.
+//   fivetranLogSchema: "fivetran_log",
+//   // Default configuration applied to all produced datasets.
+//   defaultConfig: {
+//     schema: "fivetran_log_package",
+//     tags: ["fiveran_log_package"],
+//     type: "view"
+//   },
+// });
