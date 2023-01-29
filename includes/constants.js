cb43d1898f60948389c4855b5a6f86cf6f5be903
ADMIN ADMIN <admin@acloudserve.com>
2023-01-29 17:02:38 +0000

    all models

--- includes/constants.js
+++ includes/constants.js
@@ -1,6 +1,8 @@
- const SOURCE_DATASET = "fivetran_log";
+ const SOURCE_DATASET = "fivetran_log_copy";
  const SOURCE_PROJECT = "sample1-328718";
  const fivetran_log_using_sync_alert_messages = true
+ const fivetran_log_using_transformations = false
+ const fivetran_log_using_triggers = true
  
- module.exports = { SOURCE_DATASET, SOURCE_PROJECT, fivetran_log_using_sync_alert_messages };
+ module.exports = { SOURCE_DATASET, SOURCE_PROJECT, fivetran_log_using_sync_alert_messages, fivetran_log_using_transformations, fivetran_log_using_triggers };
 //  module.exports = {  };
